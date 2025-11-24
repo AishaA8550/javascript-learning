78f101ee0364457728897eb05ef6b2d2f20387f5
AishaA8550 <aishaa3564@gmail.com>
2025-11-25 01:59:41 +0530

    feat: add external JavaScript file for cartquantity project logic

---
+++ projects/scripts/04-cartquantity.js
@@ -0,0 +1,27 @@
+
+      let cartQuantity = 0;
+
+      // Display the cart quantity when the page loads.
+      displayCartQuantity();
+
+      function updateCartQuantity(change) {
+        if (cartQuantity + change > 10) {
+          alert('The cart is full');
+          return;
+        }
+
+        if (cartQuantity + change < 0) {
+          alert('Not enough items in the cart');
+          return;
+        }
+
+        cartQuantity += change;
+
+        // Display the cart quantity after updating.
+        displayCartQuantity();
+      }
+
+      function displayCartQuantity() {
+        document.querySelector('.js-cart-quantity')
+          .innerHTML = `Cart quantity: ${cartQuantity}`;
+      }
